a3ff20fd70f44a9030675303d1e32eecb9d118f5
Ben Flood <B00145093@mytudublin.ie>
2023-12-05 00:15:54 +0000

    updated tables in sql database

--- makeFoodOrderDB.sql
+++ makeFoodOrderDB.sql
@@ -1,11 +1,27 @@
 drop database if exists orderfood;
 create database orderfood;
 use orderfood;
-create table foodorder (
+
+
+create table MainsMenu (
     id int auto_increment,
-    firstname varchar(30),
-    surname varchar(30),
-    adressLine1 varchar(30),
-    adressLine2 varchar(30),
+    Item varchar(30),
+    Price integer(10),
     primary key(id)
 );
+
+create table DessertMenu (
+    id int auto_increment,
+    Item varchar(30),
+    Price integer(10),
+    primary key(id)
+);
+
+create table DrinksMenu (
+    id int auto_increment,
+    Item varchar(30),
+    Price integer(10),
+    primary key(id)
+);
+
+
