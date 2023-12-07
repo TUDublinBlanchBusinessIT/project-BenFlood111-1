d8ce68b630265aaf3e081178239424802e056de5
Ben Flood <B00145093@mytudublin.ie>
2023-12-07 21:52:29 +0000

    updated and renamed php file

---
+++ OrderForm.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("dbcon.php")
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $mains_id = $_POST["mains_id"];
+    $desserts_id = $_POST["desserts_id"];
+    $drinks_id = $_POST["drinks_id"];
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $adressLine1 = $_POST["adressLine1"];
+    $adressLine2 = $_POST["adressLine2"];
+
+    $sql = "Insert into orders (mains_id, desserts_id, drinks_id, firstName, surName, adressLine1, adressLine2)
+    values ('$mains_id', '$desserts_id', '$drinks_id', '$firstName', '$surName', '$adressLine1', '$adressLine2')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Order successfully placed";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      
+      mysqli_close($conn);
+
+
+?>
