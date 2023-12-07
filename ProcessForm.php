279e0d7fbeb6a29628d5851be8941715a010e23d
Ben Flood <B00145093@mytudublin.ie>
2023-12-07 21:52:09 +0000

    updated and renamed php file

--- ProcessForm.php
+++ ProcessForm.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include("dbcon.php")
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -15,7 +15,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "Insert into orders (mains_id, desserts_id, drinks_id, firstName, surName, adressLine1, adressLine2)
     values ('$mains_id', '$desserts_id', '$drinks_id', '$firstName', '$surName', '$adressLine1', '$adressLine2')";
 
-
+    if (mysqli_query($conn, $sql)) {
+        echo "Order successfully placed";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      
+      mysqli_close($conn);
 
 
 ?>
